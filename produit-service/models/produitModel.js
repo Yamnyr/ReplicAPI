cb7a909be035be085309638ab2fbb12f47fbdff0
Yamnyr <quentinwaroquet@gmail.com>
2025-05-11 02:16:41 +0200

    mini fix

--- produit-service/models/produitModel.js
+++ produit-service/models/produitModel.js
@@ -1,16 +1,17 @@
 const db = require("../config/db")
 
 const getAllProduits = async () => {
-    // Utilisation du pool de lecture pour les opérations SELECT
-    const result = await db.query("SELECT * FROM produits")
-    return result.rows
-}
+    const result = await db.query('SELECT * FROM produits');
+    return result.rows;
+};
 
 const createProduit = async (name) => {
-    // Utilisation du pool d'écriture pour les opérations INSERT
-    const result = await db.writeQuery("INSERT INTO produits (name) VALUES ($1) RETURNING *", [name], "create_produit")
-    return result.rows[0]
-}
+    const result = await db.query(
+        'INSERT INTO produits (name) VALUES ($1) RETURNING *',
+        [name]
+    );
+    return result.rows[0];
+};
 
 module.exports = {
     getAllProduits,
